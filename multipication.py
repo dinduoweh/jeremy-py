c3d0d07f8b2be13e322a58d55158193acaf5b6b5
Chukwumdindu Oweh <141310678+owehjeremy1234456@users.noreply.github.com>
2023-11-20 15:25:46 +0300

    Add files via upload

---
+++ multipication.py
@@ -0,0 +1,4 @@
+# this code is generated for multiplication table 
+number= input('Enter a number to generated subtract   table ')
+for container in range ( 1,78):
+    print(number, '-', container, '=', int(number) - container)
